a0f2314a53af93241f0abd5aae9b603fb4f23443
Klaudia <klaudia@Wojteks-MacBook-Pro.local>
2018-10-15 20:36:11 +0200

    Fetching data

--- WeatherForecast/ViewController.swift
+++ WeatherForecast/ViewController.swift
@@ -12,14 +12,17 @@ class ViewController: UIViewController {
     
     //MARK: Properties
     
-    @IBOutlet weak var maxTemp: UITextField!
-    @IBOutlet weak var minTemp: UITextField!
-    @IBOutlet weak var pressure: UITextField!
-    @IBOutlet weak var precipitation: UITextField!
-    @IBOutlet weak var summary: UITextField!
-    @IBOutlet weak var windDirection: UITextField!
-    @IBOutlet weak var windSpeed: UITextField!
+    @IBOutlet weak var minTemp: UILabel!
+    @IBOutlet weak var maxTemp: UILabel!
+    @IBOutlet weak var windSpeed: UILabel!
+    @IBOutlet weak var pressure: UILabel!
+    @IBOutlet weak var precipitation: UILabel!
+    @IBOutlet weak var windDirection: UILabel!
+    @IBOutlet weak var date: UILabel!
+    @IBOutlet weak var summary: UILabel!
     @IBOutlet weak var pictogram: UIImageView!
+    
+    private var myData: [Data]?
     
     //MARK: Actions
     
@@ -31,61 +34,99 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-       data()
-       // getData()
+        getData { [weak self] weatherForecast in
+            guard let self = self, let fetchData = weatherForecast.daily?.data?.first else {
+                return
+            }
+            
+            self.myData = weatherForecast.daily?.data
+            self.setupFirst(data: fetchData)
+        }
+        
+    }
+    
+    private func setupFirst(data: Data) {
+        
+        pictogram.contentMode = .scaleAspectFit
+        pictogram.image = weatherPictogramFromString(stringIcon: data.icon)
+        maxTemp.text = String(data.temperatureMax!)
+        minTemp.text = String(data.temperatureMin!)
+        pressure.text = String(data.pressure!)
+        precipitation.text = String(data.precipIntensity!)
+        summary.text = data.summary
+        windSpeed.text = String(data.windSpeed!)
+        date.text = convertTimestampToDate(time: data.time!)
     }
     
     let weatherForecastUrl = URL(string: "https://api.darksky.net/forecast/813dcd329357b2039f8650fbf84c481e/37.8267,-122.4233")!
     
     
-    func data() {
-         let task = URLSession.shared.dataTask(with: weatherForecastUrl) {(data: Data?, response:URLResponse?, error: Error?) in
-            guard let unwrappedData = data else { return }
-            do {
-                let someDictionaryFromJSON = try JSONSerialization.jsonObject(with: unwrappedData, options: .allowFragments)     as! [String: Any]
-                let json4Swift_Base = Json4Swift_Base(someDictionaryFromJSON)
-                print(json4Swift_Base)
-            } catch {
-                print(error.localizedDescription)
+    func getData (completion: @escaping (WeatherForecastBase) -> Void) {
+        let task = URLSession.shared.dataTask(with: weatherForecastUrl) {(data, response, error) in
+            guard let fetchedData = data else {
+                return
             }
-            
+
+                do {
+                    let weatherForecast = try JSONDecoder().decode(WeatherForecastBase.self, from: fetchedData)
+                    
+                    DispatchQueue.main.async() {
+                        completion(weatherForecast)
+                    }
+                    
+                } catch {
+                    print(error.localizedDescription)
+                
+                }
         }
         task.resume()
     }
     
-    func getData() {
-
-        //to dziala asynchronicznie wiec musze to odpowiednio wywolac
-           let task = URLSession.shared.dataTask(with: weatherForecastUrl) {(data: Data?, response:URLResponse?, error: Error?) in
-            guard let unwrappedData = data else { return }
-            do {
-                //let json = try JSONSerialization.jsonObject(with: unwrappedData, options: []) as? [String:Any]
-                //let json = try JSONSerialization.jsonObject(with: unwrappedData)
-//                print(json)
-             
-                //print(type(of: json))
-//                let jsonData = json.data(using: .utf8)!
-//                let decoder = JSONDecoder()
-//                let myStruct = try! decoder.decode(WeatherForecast.self, from: jsonData)
-//                print(myStruct)
-//                let result = try? JSONDecoder().decode((WeatherForecast.self), from: unwrappedData)
-
-//                print(result)
-
-                // let jsonDecoder = JSONDecoder()
-                //TODO -> cannot convert
-                
-                 //let weatherObject = try jsonDecoder.decode(WeatherForecast.self, from: json)
-                 //print(weatherObject)
-                
-               // print(json!)
-            } catch {
-                print(error.localizedDescription)
-            }
-
+    func convertTimestampToDate(time: Int) -> String {
+        let date = Date(timeIntervalSince1970: TimeInterval(time))
+        let dateFormatter = DateFormatter()
+        dateFormatter.timeStyle = DateFormatter.Style.medium
+        dateFormatter.dateStyle = DateFormatter.Style.medium
+        let localDate = dateFormatter.string(from: date)
+        
+        return localDate
+    }
+   
+    func weatherPictogramFromString(stringIcon: String?) -> UIImage {
+        guard let stringIcon = stringIcon else {
+            return UIImage(named: "clear-day")!
         }
-
-        task.resume()
+        
+        var imageName: String
+        
+        switch stringIcon {
+        case "clear-day":
+            imageName = "clear-day"
+        case "clear-night":
+            imageName = "clear-night"
+        case "rain":
+            imageName = "rain"
+        case "snow":
+            imageName = "snow"
+        case "sleet":
+            imageName = "sleet"
+        case "wind":
+            imageName = "wind"
+        case "fog":
+            imageName = "fog"
+        case "cloudy":
+            imageName = "cloudy"
+        case "partly-cloudy-day":
+            imageName = "partly-cloudy"
+        case "partly-cloudy-night":
+            imageName = "cloudy-night"
+        default:
+            imageName = "clear-day"
+        }
+        
+        let iconImage = UIImage(named: imageName)
+        return iconImage!
+       
     }
 }
 
